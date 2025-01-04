1a402293ce314f8db55fb369b2cb555adeaa434b
ehan03 <eugeneh442@gmail.com>
2025-01-03 19:38:15 -0500

    maybe this?

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -33,7 +33,7 @@ st.markdown(
 st.title("🎮 Steam Time Series 📈")
 
 # Create the tabs
-spacing = "\u2001\u2001\u2001"
+spacing = "\u2001 \u2001 \u2001"
 tab1, tab2, tab3, tab4 = st.tabs(
     [
         f"{spacing}  Home  {spacing}",
